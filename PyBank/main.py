6e754a7d529ccebce336be82e0181d540153d543
Praise1018 <praise101837@gmail.com>
2024-05-17 00:23:07 -0400

    PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,13 +3,42 @@ import csv
 
 csvpath = os.path.join("PyBank/Resources/budget_data.csv")
 
+totalmonths = 0
+totalmonths += 0
+nettotal = 0
+averagechange = 0
+greatestincrease = 0
+greatestdecrease = 0
+
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    next(csvreader)
+    csvheader = next(csvreader)
 
-print("Financial Analysis")
+    for row in csvreader:
+       
+       totalmonths += 1
+       nettotal += int(row[1])
 
-print("----------------------------")
+       greatestincrease = max(row[1])
+       greatestdecrease = min(row[1])
+       
+averagechange = round(nettotal/totalmonths)
+
+print(f"Total Months: {totalmonths}")
+print(f"Total: ${nettotal}")
+print(f"Average Change: ${averagechange}")
+print(f"Greatest Increase: {greatestincrease}")
+print(f"Greatest Decrease: {greatestdecrease}")
+
+output_file = "analysis.txt"
+
+with open(output_file, "w") as file:
+    file.write(f"Total Months: {totalmonths}")
+    file.write(f"Total: ${nettotal}")
+    file.write(f"Average Change: ${averagechange}")
+    file.write(f"Greatest Increase: {greatestincrease}")
+    file.write(f"Greatest Decrease: {greatestdecrease}")
+
 
